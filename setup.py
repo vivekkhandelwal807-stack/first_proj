640fd067b12376a5d032ec17dd8daa5c0283770f
Vivek <vivekkhandelwal807@gmail.com>
2026-03-14 15:57:12 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+
+
+
+
+
+
+
+
+setup(
+
+    name='mlproject',
+    version='0.0.1',
+    author='vivek',
+    packages=find_packages(),
+    install_requires=['numpy','pandas'],
+
+
+)
